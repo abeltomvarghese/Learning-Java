95bc103ee1bf59a70fd69a8b3510e59ac4b67b5f
unknown <abeltomvarghese1998@gmail.com>
2020-01-29 12:31:22 +0000

    collection algorithms

---
+++ Java-Collections/src/com/CollectionOperations/Main.java
@@ -0,0 +1,41 @@
+package com.CollectionOperations;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(ProductList.table);
+        products.add(ProductList.wardrobe);
+        products.add(ProductList.door);
+        products.add(ProductList.sofa);
+
+        System.out.println(products);
+        Collections.rotate(products,1);
+        System.out.println(products);
+        Collections.rotate(products,1);
+        System.out.println(products);
+        Collections.rotate(products,1);
+        System.out.println(products);
+        Collections.rotate(products,1);
+        System.out.println(products);
+
+
+        System.out.println("SHUFFLING");
+        Collections.shuffle(products);
+        System.out.println(products);
+        Collections.shuffle(products);
+        System.out.println(products);
+
+        System.out.println("SORTING");
+        products.sort(Product.BY_WEIGHT);
+        System.out.println(products);
+
+    }
+}
---
+++ Java-Collections/src/com/CollectionOperations/Product.java
@@ -0,0 +1,71 @@
+package com.CollectionOperations;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+import static java.util.Comparator.comparing;
+
+public class Product implements Comparable<Product>{
+    private int id;
+    private String name;
+    private int weight;
+
+    public static final Comparator<Product> BY_NAME = comparing(Product::getName);
+    public static final Comparator<Product> BY_WEIGHT = comparing(Product::getWeight);
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ",name,":",Integer.toString(weight));
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return this.getWeight() < o.getWeight() ? -1 : this.getWeight() > o.getWeight() ? 1 : 0;
+        
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return weight == product.weight &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, weight);
+    }
+
+
+}
