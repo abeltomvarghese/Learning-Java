a2f9e05a2bce6e2919f1db4506208e6878a86be6
unknown <abeltomvarghese1998@gmail.com>
2020-01-29 12:41:42 +0000

    unmodifiable views with mutable list

---
+++ Java-Collections/src/com/CollectionOperations/ShoppingBasket.java
@@ -0,0 +1,31 @@
+package com.CollectionOperations;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ShoppingBasket {
+    private final List<Product> items = new ArrayList<>();
+    private int totalWeight = 0;
+
+    public void add (Product product) {
+        items.add(product);
+        totalWeight += product.getWeight();
+    }
+    public List<Product> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    @Override
+    public String toString() {
+        return "Shopping Basket of \n" + items + "\nwith weight: " + totalWeight;
+    }
+
+    public static void main(String[] args) {
+        ShoppingBasket shoppingBasket = new ShoppingBasket();
+        shoppingBasket.add(ProductList.door);
+        shoppingBasket.getItems().add(ProductList.sofa);
+    }
+}
