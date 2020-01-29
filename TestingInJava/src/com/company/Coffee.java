aaf23e12d93f3c9138cda5319eaa93786afe4db8
unknown <abeltomvarghese1998@gmail.com>
2020-01-29 16:38:19 +0000

    starting code for unit tests

---
+++ TestingInJava/src/com/company/Coffee.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Coffee {
+
+    private final CoffeeType type;
+    private final int beans;
+    private final int milk;
+
+    public Coffee(CoffeeType type, int beans, int milk) {
+        this.type = type;
+        this.beans = beans;
+        this.milk = milk;
+    }
+
+    public CoffeeType getType() {return this.type;}
+    public int getBeans() {return beans;}
+    public int getMilk() {return milk;}
+
+    @Override
+    public String toString() {
+        return "Coffee{" + "type=" + type + ", beans=" + beans + ", milk=" + milk + '}';
+    }
+
+}
