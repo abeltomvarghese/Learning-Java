e68b97c4170f3e8fa370e6e310bc11ee39708484
unknown <abeltomvarghese1998@gmail.com>
2020-02-01 12:35:41 +0000

    using @BeforeEach to replace constructor or shared methods

--- TestingInJava/Tests/com/company/CafeTest.java
+++ TestingInJava/Tests/com/company/CafeTest.java
@@ -1,60 +1,70 @@
 package com.company;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CafeTest {
     private final int ESPRESSO_BEANS = 7;
     private final int EMPTY_STOCK = 0;
+
+    private Cafe testCafe;
+
+    @BeforeEach
+    public void before() {
+        testCafe = new Cafe();
+    }
+
+    private Cafe initBeans() {
+        testCafe.restockBeans(ESPRESSO_BEANS);
+        return testCafe;
+    }
+
+
+
     @Test
     public void canBrewExpresso() {
-        Cafe cafe = new Cafe();
-        cafe.restockBeans(ESPRESSO_BEANS);
+        initBeans();
 
-        Coffee testCoffee = cafe.brew(CoffeeType.Espresso);
+        Coffee testCoffee = testCafe.brew(CoffeeType.Espresso);
 
 
         //check whether the coffee is an espresso
-        Assertions.assertEquals(CoffeeType.Espresso,testCoffee.getType());
+        Assertions.assertEquals(CoffeeType.Espresso,testCoffee.getType(), "Wrong type of coffee");
 
         //check whether the coffee has milk
-        Assertions.assertEquals(EMPTY_STOCK,testCoffee.getMilk());
+        Assertions.assertEquals(EMPTY_STOCK,testCoffee.getMilk(),"Wrong amount of milk");
 
         //check how many coffee beans
-        Assertions.assertEquals(ESPRESSO_BEANS, testCoffee.getBeans());
+        Assertions.assertEquals(ESPRESSO_BEANS, testCoffee.getBeans(),"Wrong amount of coffee beans");
     }
 
     @Test
     public void brewingEspressoConsumesBeans() {
-        Cafe testCafe = new Cafe();
-        testCafe.restockBeans(7);
+        initBeans();
 
         Coffee testCoffee = testCafe.brew(CoffeeType.Espresso);
 
-        Assertions.assertEquals(EMPTY_STOCK, testCafe.getBeansInStock());
+        Assertions.assertEquals(EMPTY_STOCK, testCafe.getBeansInStock(),"Wrong number of coffee in cafe");
     }
 
     @Test
     public void lattesRequiresMilk() {
-        Cafe testCafe = new Cafe();
-        testCafe.restockBeans(ESPRESSO_BEANS);
+        initBeans();
 
         Assertions.assertThrows(IllegalStateException.class, () -> {
             Coffee coffee = testCafe.brew(CoffeeType.Latte);
-        });
+        },"Not enough milk or coffee beans");
     }
 
     @Test
     public void canBrewLatte() {
-        Cafe testCafe = new Cafe();
-        testCafe.restockBeans(ESPRESSO_BEANS);
+        initBeans();
         testCafe.restockMilk(227);
 
         Coffee testCoffee = testCafe.brew(CoffeeType.Latte);
 
-        Assertions.assertEquals(227, testCoffee.getMilk(),"Wrong Amount of Milk");
+        Assertions.assertEquals(CoffeeType.Latte.getRequiredMilk(), testCoffee.getMilk(),"Wrong Amount of Milk");
         Assertions.assertEquals(CoffeeType.Latte, testCoffee.getType(),"Wrong Type of Coffee");
 
     }
