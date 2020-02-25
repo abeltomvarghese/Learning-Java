4f78c148a7ae2e7b11372a7a2c5dd39a15696961
unknown <abeltomvarghese1998@gmail.com>
2020-02-25 13:38:54 +0000

    serializing and deserializing an object - persisting

--- javaCorePlatform/src/SerialzationDeserialization/BankAccount.java
+++ javaCorePlatform/src/SerialzationDeserialization/BankAccount.java
@@ -1,4 +1,29 @@
 package SerialzationDeserialization;
 
-public class BankAccount {
+import java.io.Serializable;
+
+public class BankAccount implements Serializable {
+
+    private final String id;
+    private int balance = 0;
+
+    public BankAccount(String id) {this.id = id;}
+
+    public BankAccount(String id, int balance) {this.id = id; this.balance = balance;}
+
+    public synchronized void deposit(int amt) {
+        this.balance += amt;
+    }
+
+    public synchronized void withdrawal(int amt) {
+        this.balance -= amt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
--- javaCorePlatform/src/SerialzationDeserialization/Main.java
+++ javaCorePlatform/src/SerialzationDeserialization/Main.java
@@ -1,4 +1,41 @@
 package SerialzationDeserialization;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+
+    public static void main(String[] args) {
+        BankAccount acct = new BankAccount("1234",500);
+        acct.deposit(250);
+        saveAccount(acct, "accounts.dat");
+        BankAccount ba = loadAccount("accounts.dat");
+        System.out.println(ba.getId() + " : " + ba.getBalance());
+    }
+
+    public static void saveAccount(BankAccount account, String filename) {
+
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(Paths.get(filename)))) {
+            objectOutputStream.writeObject(account);
+        } catch (IOException ioE) {
+            ioE.printStackTrace();
+        }
+    }
+
+    public static BankAccount loadAccount(String fileName) {
+        BankAccount ba = null;
+        try(ObjectInputStream inputStream = new ObjectInputStream(Files.newInputStream(Paths.get(fileName)))) {
+            ba = (BankAccount) inputStream.readObject();
+        } catch (IOException ioE) {
+            ioE.printStackTrace();
+        } catch (ClassNotFoundException cnfE) {
+            cnfE.printStackTrace();
+        }
+        return ba;
+    }
+
 }
