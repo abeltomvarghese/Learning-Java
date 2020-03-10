82c1e62d7ad6e667a1b3651e3c8bf30329abe436
unknown <abeltomvarghese1998@gmail.com>
2020-03-10 21:28:31 +0000

    learnt how to make rest calls in java

---
+++ HttpClientData/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+
+    private static HttpClient client;
+    public static void main(String[] args) throws IOException {
+
+        client = HttpClient.newHttpClient();
+
+        Files.lines(Path.of("urls.txt"))
+                .map(Main::validateLink)
+                .forEach(System.out::println);
+
+    }
+
+    public static String validateLink(String link) {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(link)).GET().build();
+
+        try {
+            HttpResponse<Void> response = client.send(request,HttpResponse.BodyHandlers.discarding());
+            return responseToString(response);
+        } catch (IOException | InterruptedException e) {
+            return String.format("%s -> %s", link,false);
+        }
+    }
+
+    public static String responseToString(HttpResponse<Void> response ) {
+        int status = response.statusCode();
+        boolean success = status >= 200 && status <= 299;
+        return String.format("%s -> %s (status: %s)", response.uri(),success, status);
+    }
+}
