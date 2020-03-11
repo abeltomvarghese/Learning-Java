963163bcaaf4b3d7729ad470053c7d985ed2f241
unknown <abeltomvarghese1998@gmail.com>
2020-03-11 12:30:30 +0000

    sending data async

--- HttpClientData/src/com/company/Main.java
+++ HttpClientData/src/com/company/Main.java
@@ -7,6 +7,8 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,21 +17,30 @@ public class Main {
 
         client = HttpClient.newHttpClient();
 
-        Files.lines(Path.of("urls.txt"))
+        var futures = Files.lines(Path.of("urls.txt"))
                 .map(Main::validateLink)
+                .collect(Collectors.toList());
+
+        futures.stream()
+                .map(CompletableFuture::join)
                 .forEach(System.out::println);
 
     }
 
-    public static String validateLink(String link) {
+    public static CompletableFuture<String> validateLink(String link) {
         HttpRequest request = HttpRequest.newBuilder(URI.create(link)).GET().build();
 
-        try {
+        /*try {
             HttpResponse<Void> response = client.send(request,HttpResponse.BodyHandlers.discarding());
             return responseToString(response);
         } catch (IOException | InterruptedException e) {
             return String.format("%s -> %s", link,false);
-        }
+        }*/
+
+        return client.sendAsync(request,HttpResponse.BodyHandlers.discarding())
+                .thenApply(Main::responseToString)
+                .exceptionally(e -> String.format("%s -> %s", link,false));
+
     }
 
     public static String responseToString(HttpResponse<Void> response ) {
